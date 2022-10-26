aa535874fba896c37c479fa6b156455ed9591f4a
Dawid <dawidpawlak268@gmail.com>
2022-10-26 20:26:04 +0200

    add db connector

--- src/features/index.ts
+++ src/features/index.ts
@@ -1,3 +1,6 @@
 export * from './article/article.module';
+export * from './article/entities/article.entity';
 export * from './article-tags/article-tags.module';
+export * from './article-tags/entity/article-tag.entity';
 export * from './article-translations/article-translations.module';
+export * from './article-translations/entity/article-translation.entity';
